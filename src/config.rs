0c4fbfa666bd7e172ab08f4a6fcb0485ddcfb715
dzania <artur.kantorczyk@gmail.com>
2022-06-08 13:00:01 +0200

    Create rue conf directory at home path

--- src/config.rs
+++ src/config.rs
@@ -1,28 +1,34 @@
 use crate::errors::ConfigError;
 use serde::{Deserialize, Serialize};
-use std::fs;
-use std::io::Write;
+use std::{
+    fs,
+    io::Write,
+    path::{Path, PathBuf},
+};
 
-static FILE_PATH: &str = ".config/rue/rue.conf";
+static FILE_PATH: &str = ".config/rue";
+static CONFIG_NAME: &str = "rue.conf";
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct User {
     username: String,
 }
+
 impl User {
+    pub fn create_path(&self) -> Result<(), ConfigError> {
+        let home_dir = dirs::home_dir().ok_or(ConfigError::HomeDirectoryNotFound(
+            "Can't find home directory".into(),
+        ))?;
+        let path = Path::new(&home_dir.join(FILE_PATH)).to_owned();
+        fs::create_dir_all(&path).map_err(|e| ConfigError::CreateFileError(e.to_string()))?;
+        Ok(())
+    }
     // Store username(token) used for api calls
     pub async fn save(&self) -> Result<(), ConfigError> {
-        let mut file = fs::File::create(
-            dirs::home_dir()
-                .ok_or(ConfigError::HomeDirectoryNotFound(
-                    "Can't find home directory".into(),
-                ))?
-                .join(FILE_PATH),
-        )
-        .map_err(|e| ConfigError::CreateFileError(e.to_string()))?;
-        let data =
-            serde_json::to_string(self).map_err(|e| ConfigError::Serialization(e.to_string()))?;
-        file.write_all(data.as_bytes())
+        self.create_path()?;
+        let mut file = fs::File::create(format!("{}/{}", FILE_PATH, CONFIG_NAME))
+            .map_err(|e| ConfigError::CreateFileError(e.to_string()))?;
+        file.write_all(&self.username.as_bytes())
             .map_err(|e| ConfigError::CreateFileError(e.to_string()))?;
 
         Ok(())
@@ -38,10 +44,7 @@ impl User {
                 .join(FILE_PATH),
         )
         .map_err(|e| ConfigError::FileReadError(e.to_string()))?;
-        let user = User {
-            username: serde_json::from_str(&username)
-                .map_err(|e| ConfigError::Deserialization(e.to_string()))?,
-        };
+        let user = User { username };
         Ok(user)
     }
 }
--- src/errors.rs
+++ src/errors.rs
@@ -10,11 +10,13 @@ pub enum ConfigError {
 impl fmt::Debug for ConfigError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match &*self {
-            ConfigError::CreateFileError(msg) => write!(f, "GRPC error: {}", msg),
-            ConfigError::FileReadError(msg) => write!(f, "LDAP error: {}", msg),
-            ConfigError::HomeDirectoryNotFound(msg) => write!(f, "Object not found: {}", msg),
+            ConfigError::CreateFileError(msg) => write!(f, "Create file error: {}", msg),
+            ConfigError::FileReadError(msg) => write!(f, "Create config error: {}", msg),
+            ConfigError::HomeDirectoryNotFound(msg) => {
+                write!(f, "Home directory not found: {}", msg)
+            }
             ConfigError::Serialization(msg) => write!(f, "Serialization error: {}", msg),
-            ConfigError::Deserialization(msg) => write!(f, "Authorization error: {}", msg),
+            ConfigError::Deserialization(msg) => write!(f, "Deserialization error: {}", msg),
         }
     }
 }
