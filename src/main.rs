b70784ebfca7454f22739b68d2fa37ecb1c28f89
dzania <artur.kantorczyk@gmail.com>
2022-07-13 22:03:45 +0200

    fix: merge conflicts

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,13 @@ mod lights;
 use std::path::Path;
 
 #[tokio::main]
-async fn main()  {
-    tracing_subscriber::fmt()
-        .with_max_level(LevelFilter::DEBUG)
-        .init();
+async fn main() {
     match Path::new(&dirs::home_dir().unwrap().join(".config/rue/rue.conf")).exists() {
         true => {
-            debug!("Config file exists");
             println!("Config file exists");
         }
         false => {
             bridge::create_user().await.unwrap();
         }
     }
-    Ok(())
 }
