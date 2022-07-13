8a82184cb999d5e95db27dc8caa769a0fb9b5067
dzania <artur.kantorczyk@gmail.com>
2022-07-13 21:59:05 +0200

    fix: wrong path on save so cant save config

--- src/config.rs
+++ src/config.rs
@@ -6,8 +6,8 @@ use std::{
     path::{Path, PathBuf},
 };
 
-static FILE_PATH: &str = ".config/rue";
-static CONFIG_NAME: &str = "rue.conf";
+const FILE_PATH: &str = ".config/rue";
+const CONFIG_NAME: &str = "rue.conf";
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct User {
@@ -15,10 +15,15 @@ pub struct User {
 }
 
 impl User {
-    pub fn create_path(&self) -> Result<(), ConfigError> {
+    fn get_home_dir() -> Result<PathBuf, ConfigError> {
         let home_dir = dirs::home_dir().ok_or(ConfigError::HomeDirectoryNotFound(
             "Can't find home directory".into(),
         ))?;
+        Ok(home_dir)
+    }
+
+    pub fn create_path(&self) -> Result<(), ConfigError> {
+        let home_dir = User::get_home_dir()?;
         let path = Path::new(&home_dir.join(FILE_PATH)).to_owned();
         fs::create_dir_all(&path).map_err(|e| ConfigError::CreateFileError(e.to_string()))?;
         Ok(())
@@ -26,24 +31,23 @@ impl User {
     // Store username(token) used for api calls
     pub async fn save(&self) -> Result<(), ConfigError> {
         self.create_path()?;
-        let mut file = fs::File::create(format!("{}/{}", FILE_PATH, CONFIG_NAME))
-            .map_err(|e| ConfigError::CreateFileError(e.to_string()))?;
+        let home_dir = User::get_home_dir()?;
+        let mut file = fs::File::create(
+            // FIXME: DRY try to remove it and get home dir as const
+            home_dir.join(FILE_PATH).join(CONFIG_NAME),
+        )
+        .map_err(|e| ConfigError::CreateFileError(e.to_string()))?;
         file.write_all(&self.username.as_bytes())
             .map_err(|e| ConfigError::CreateFileError(e.to_string()))?;
+        println!("User saved");
 
         Ok(())
     }
 
     // Load username(token) used for api calls
     pub async fn load() -> Result<Self, ConfigError> {
-        let username = fs::read_to_string(
-            dirs::home_dir()
-                .ok_or(ConfigError::HomeDirectoryNotFound(
-                    "Can't find home directory".into(),
-                ))?
-                .join(FILE_PATH),
-        )
-        .map_err(|e| ConfigError::FileReadError(e.to_string()))?;
+        let username = fs::read_to_string(User::get_home_dir()?.join(FILE_PATH))
+            .map_err(|e| ConfigError::FileReadError(e.to_string()))?;
         let user = User { username };
         Ok(user)
     }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
             println!("Config file exists");
         }
         false => {
-            bridge::create_user().await;
+            bridge::create_user().await.unwrap();
         }
     }
 }
