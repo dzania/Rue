39ae760aed9003502896e25a41db65b127633c49
dzania <artur.kantorczyk@gmail.com>
2022-07-16 14:42:11 +0200

    feat: setup tui

--- src/bridge.rs
+++ src/bridge.rs
@@ -18,83 +18,87 @@ pub enum BridgeErrors {
     NoBridgesFound,
 }
 
-/// find bridges using discovery url
-pub async fn find_bridges() -> Result<Vec<Bridge>, Error> {
-    let request: Vec<Bridge> = reqwest::get("https://discovery.meethue.com/")
-        .await?
-        .json()
-        .await?;
-    if request.is_empty() {
-        panic!("No bridges found");
+impl Bridge {
+    /// find bridges using discovery url
+    pub async fn find_bridges() -> Result<Vec<Self>, Error> {
+        let request: Vec<Bridge> = reqwest::get("https://discovery.meethue.com/")
+            .await?
+            .json()
+            .await?;
+        if request.is_empty() {
+            panic!("No bridges found");
+        }
+        Ok(request)
     }
-    Ok(request)
-}
 
-/// Send parallel requests to all bridges found
-pub async fn create_user() -> Result<(), ConfigError> {
-    let ips: Vec<String> = find_bridges()
-        .await
-        .expect("No bridges found")
-        .into_iter()
-        .map(|bridge| bridge.internalipaddress)
-        .collect();
+    /// Send parallel requests to all bridges found
+    pub async fn create_user() -> Result<(), ConfigError> {
+        let ips: Vec<String> = Bridge::find_bridges()
+            .await
+            .expect("No bridges found")
+            .into_iter()
+            .map(|bridge| bridge.internalipaddress)
+            .collect();
 
-    let mut counter = 0;
-    while counter < 25 {
-        let (tx, mut rx) = mpsc::channel(4);
-        let requests = stream::iter(ips.clone())
-            .map(|ip| {
-                tokio::spawn(async move {
-                    let resp = authorize_user_request(&ip).await;
-                    resp
+        let mut counter = 0;
+        while counter < 25 {
+            let (tx, mut rx) = mpsc::channel(4);
+            let requests = stream::iter(ips.clone())
+                .map(|ip| {
+                    tokio::spawn(async move {
+                        let resp = Bridge::authorize_user_request(&ip).await;
+                        resp
+                    })
                 })
-            })
-            .buffer_unordered(ips.len());
+                .buffer_unordered(ips.len());
 
-        requests
-            .for_each(|b| async {
-                match b {
-                    Ok(Ok(b)) => {
-                        let _ = tx.send(b).await;
+            requests
+                .for_each(|b| async {
+                    match b {
+                        Ok(Ok(b)) => {
+                            let _ = tx.send(b).await;
+                        }
+                        // FIXME: Shouldn't print to std
+                        Ok(Err(e)) => println!("Got a reqwest::Error: {}", e),
+                        Err(e) => println!("Error: {}", e),
                     }
-                    // FIXME: Shouldn't print to std
-                    Ok(Err(e)) => println!("Got a reqwest::Error: {}", e),
-                    Err(e) => println!("Error: {}", e),
+                })
+                .await;
+
+            if let Some(message) = rx.recv().await {
+                if let Ok(user) = Bridge::handle_authorize_response(message).await {
+                    user.save().await?;
+                    break;
                 }
-            })
-            .await;
-
-        if let Some(message) = rx.recv().await {
-            if let Ok(user) = handle_authorize_response(message).await {
-                user.save().await?;
-                break;
             }
+            thread::sleep(Duration::from_secs(4));
+            counter += 1;
         }
-        thread::sleep(Duration::from_secs(4));
-        counter += 1;
+        Ok(())
     }
-    Ok(())
-}
 
-/// Send request to bridge to get User
-pub async fn authorize_user_request(ip: &str) -> Result<serde_json::Value, Error> {
-    let address = format!("http://{}/api", ip);
-    let client = Client::new();
-    let mut body = HashMap::new();
-    body.insert("devicetype", "rue_pc_app");
-    let resp = client.post(&address).json(&body).send().await?;
-    let data = resp.text().await?;
-    let value: Value = serde_json::from_str(&data).unwrap();
-    Ok(value)
-}
+    /// Send request to bridge to get User
+    pub async fn authorize_user_request(ip: &str) -> Result<serde_json::Value, Error> {
+        let address = format!("http://{}/api", ip);
+        let client = Client::new();
+        let mut body = HashMap::new();
+        body.insert("devicetype", "rue_pc_app");
+        let resp = client.post(&address).json(&body).send().await?;
+        let data = resp.text().await?;
+        let value: Value = serde_json::from_str(&data).unwrap();
+        Ok(value)
+    }
 
-// TODO: Reasonable error
-pub async fn handle_authorize_response(message: serde_json::Value) -> Result<User, BridgeErrors> {
-    match message[0].get("success") {
-        Some(message) => {
-            let user: User = serde_json::from_value(message.to_owned()).unwrap();
-            Ok(user)
+    // TODO: Reasonable error
+    pub async fn handle_authorize_response(
+        message: serde_json::Value,
+    ) -> Result<User, BridgeErrors> {
+        match message[0].get("success") {
+            Some(message) => {
+                let user: User = serde_json::from_value(message.to_owned()).unwrap();
+                Ok(user)
+            }
+            None => Err(BridgeErrors::ButtonNotPressed),
         }
-        None => Err(BridgeErrors::ButtonNotPressed),
     }
 }
---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+pub mod api;
+pub mod bridge;
+pub mod config;
+pub mod errors;
+pub mod lights;
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io;
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders, Widget},
+    Terminal,
+};
+
+pub fn start_ui() -> Result<(), io::Error> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+    Ok(())
+}
