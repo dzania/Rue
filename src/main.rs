6660bae971f238d12b9f330998d755873e02b1ac
Artur Kantorczyk <64010779+dzania@users.noreply.github.com>
2022-07-20 22:23:13 +0200

    feat: setup tui (#6)
    * feat: setup tui

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
-mod api;
-mod bridge;
-mod config;
-mod errors;
-mod lights;
+use rue::{bridge::Bridge, start_ui};
 use std::path::Path;
 
 #[tokio::main]
 async fn main() {
     match Path::new(&dirs::home_dir().unwrap().join(".config/rue/rue.conf")).exists() {
         true => {
-            println!("Config file exists");
+            start_ui();
         }
         false => {
-            bridge::create_user().await.unwrap();
+            Bridge::create_user().await.unwrap();
         }
     }
 }
