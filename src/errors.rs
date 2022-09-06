aa0ec89ae237145b866dd66cef74265aa90622bc
Artur Kantorczyk <64010779+dzania@users.noreply.github.com>
2022-09-06 20:05:03 +0200

    refactor: change user struct to hold info about bridge adress and move bridge errors to errors file (#10)

--- src/errors.rs
+++ src/errors.rs
@@ -4,14 +4,52 @@ pub enum ConfigError {
     CreateFileError(String),
     FileReadError(String),
     HomeDirectoryNotFound(String),
+    Serialize(String),
+    CreateDirectoryError(String),
 }
 impl fmt::Debug for ConfigError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            ConfigError::CreateFileError(msg) => write!(f, "Create file error: {}", msg),
-            ConfigError::FileReadError(msg) => write!(f, "Create config error: {}", msg),
+            ConfigError::CreateFileError(msg) => {
+                write!(f, "CONFIG_ERROR Create file error: {}", msg)
+            }
+            ConfigError::FileReadError(msg) => {
+                write!(f, "CONFIG_ERROR Create config error: {}", msg)
+            }
             ConfigError::HomeDirectoryNotFound(msg) => {
-                write!(f, "Home directory not found: {}", msg)
+                write!(f, "CONFIG_ERROR Home directory not found: {}", msg)
+            }
+            ConfigError::Serialize(msg) => {
+                write!(f, "CONFIG_ERROR Serialization error: {}", msg)
+            }
+            ConfigError::CreateDirectoryError(msg) => {
+                write!(f, "CONFIG_ERROR create config dirctory: {}", msg)
+            }
+        }
+    }
+}
+
+pub enum BridgeError {
+    ButtonNotPressed,
+    NoBridgesFound,
+    RequestError(String),
+    ResponseError(String),
+}
+
+impl fmt::Debug for BridgeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BridgeError::ButtonNotPressed => {
+                write!(f, "{:?}", self)
+            }
+            BridgeError::NoBridgesFound => {
+                write!(f, "{:?}", self)
+            }
+            BridgeError::RequestError(msg) => {
+                write!(f, "Request error: {}", msg)
+            }
+            BridgeError::ResponseError(msg) => {
+                write!(f, "Response error: {}", msg)
             }
         }
     }
